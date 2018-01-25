963639c297ecf12cf30a88b55743d1a45f4b2005
dantleech <dan.t.leech@gmail.com>
2018-01-25 11:06:39 +0100

    Added another test case

--- tests/Unit/Disqus/RemoteAuthSerializerTest.php
+++ tests/Unit/Disqus/RemoteAuthSerializerTest.php
@@ -44,6 +44,12 @@ class RemoteAuthSerializerTest extends TestCase
                 'daniel@example.com',
                 'eyJpZCI6IjNjNzcwNTc4LWI2ZmItMTFlNi1iZGM4LTAyNDJhYzExMDAwNCIsInVzZXJuYW1lIjoiRGFuaWVsIiwiZW1haWwiOiJkYW5pZWxAZXhhbXBsZS5jb20ifQ== d0b40089f325842c9a64ea83e6729b841924fb53 1516870506',
             ],
+            [
+                '1cb578ee-a047-4603-83b5-5b908c6639a6',
+                'registered@test.com',
+                'registered@test.com',
+                'eyJpZCI6IjFjYjU3OGVlLWEwNDctNDYwMy04M2I1LTViOTA4YzY2MzlhNiIsInVzZXJuYW1lIjoicmVnaXN0ZXJlZEB0ZXN0LmNvbSIsImVtYWlsIjoicmVnaXN0ZXJlZEB0ZXN0LmNvbSJ9 427c68e3c3b439005ca7db9b6005edecbe4a685c 1516870506',
+            ]
         ];
     }
 }
